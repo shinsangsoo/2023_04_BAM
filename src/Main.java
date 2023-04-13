eefa14ef0423e66e3a06cd19d162cad4af632280
shinsangsoo <sangsoo9400@gmail.com>
2023-04-13 21:31:17 +0900

    article write 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 == ");
-		
+
 		Scanner sc = new Scanner(System.in);
+
 		
+		int lastArticleId = 0;
 		
+		List<Article> articles = new ArrayList<>();
 		
-		while(true) {
-			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+		while (true) {
 			
-			if(cmd.equals("exit")) {
+
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();		
+		
+			if (cmd.equals("exit")) {
 				break;
 			}
+			
+			if(cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(id, title, body);
+				
+				articles.add(article);
+				
+				System.out.printf("%d번 글이 생성되었습니다\n", id);
+			} 
 		}
-		
+
 		sc.close();
-		
+
 		System.out.println("== 프로그램 끝 == ");
 	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+	
 }
