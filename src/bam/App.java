9f592dd623bf31fd53145efec604dd20a59b2413
shinsangsoo <sangsoo9400@gmail.com>
2023-04-18 20:22:38 +0900

    article list 검색 기능 구현

--- src/bam/App.java
+++ src/bam/App.java
@@ -47,16 +47,38 @@ public class App {
 
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
 
-			} else if (cmd.equals("article list")) {
+			} else if (cmd.startsWith("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("존재하는 게시물이 없습니다");
 					continue;
 				}
+				
+				List<Article> printArticles = articles;
+				
+				String searchKeyword = cmd.substring("article list".length()).trim();
+				
+				if(searchKeyword.length() > 0) {
+					System.out.println("검색어 : " + searchKeyword);
+					
+					printArticles = new ArrayList<>();
+					
+					for(Article article : articles) {
+						if(article.title.contains(searchKeyword)) {
+							printArticles.add(article);
+						}
+					}
+					
+					if(printArticles.size() == 0) {
+						System.out.println("검색결과가 없습니다");
+						continue;
+					}
+				}
+				
 				System.out.println("== 게시물 목록 ==");
 				System.out.println("번호	|	제목	|	작성일	");
 
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+				for (int i = printArticles.size() - 1; i >= 0; i--) {
+					Article article = printArticles.get(i);
 
 					System.out.printf("%d	|	%s	|	%s	\n", article.id, article.title, article.regDate);
 				}
