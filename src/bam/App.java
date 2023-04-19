bbaaf82353ecc1fb0ea2dae8ebd06a805f2652cf
shinsangsoo <sangsoo9400@gmail.com>
2023-04-19 23:05:45 +0900

    회원가입 기능 구현, 로그인 아이디 중복체크, 비밀번호 체크

--- src/bam/App.java
+++ src/bam/App.java
@@ -5,15 +5,20 @@ import java.util.List;
 import java.util.Scanner;
 
 import bam.dto.Article;
+import bam.dto.Member;
 import bam.util.Util;
 
 public class App {
 	private List<Article> articles;
+	private List<Member> members;
 	private int lastArticleId;
+	private int lastMemberId;
 	
 	public App() {
 		articles = new ArrayList<>();
+		members = new ArrayList<>();
 		lastArticleId = 0;
+		lastMemberId = 0;
 	}
 	
 	public void run() {
@@ -32,7 +37,60 @@ public class App {
 				break;
 			}
 
-			if (cmd.equals("article write")) {
+			if (cmd.equals("member join")) {
+				System.out.println("== 회원 가입 ==");
+				int id = lastMemberId + 1;
+				lastMemberId = id;
+				
+				String loginId = null;
+				
+				while(true) {
+					System.out.printf("로그인 아이디 : ");
+					loginId = sc.nextLine();
+					
+					if(isLoginIdDup(loginId) == false) {
+						System.out.printf("%s은(는) 이미 사용중인 아이디입니다\n", loginId);
+						continue;
+					}
+					
+					System.out.printf("%s은(는) 사용가능한 아이디입니다\n", loginId);
+					break;
+				}
+				
+				String loginPw = null;
+				
+				while(true) {
+					System.out.printf("로그인 비밀번호 : ");
+					loginPw = sc.nextLine();
+					System.out.printf("로그인 비밀번호 확인 : ");
+					String loginPwChk = sc.nextLine();
+					
+					if (loginPw.equals(loginPwChk) == false) {
+						System.out.println("비밀번호를 확인해주세요");
+						continue;
+					}
+					
+					break;
+				}
+				
+				System.out.printf("이름 : ");
+				String name = sc.nextLine();
+
+				Member member = new Member(id, Util.getDateStr(), loginId, loginPw, name);
+
+				members.add(member);
+
+				System.out.printf("%s회원님이 가입되었습니다\n", loginId);
+
+			} else if (cmd.equals("member list")) {
+				
+				for (int i = members.size() - 1; i >= 0; i--) {
+					Member member = members.get(i);
+
+					System.out.printf("%d	|	%s	|	%s	|	%s	\n", member.id, member.loginId, member.regDate, member.name);
+				}
+				
+			} else if (cmd.equals("article write")) {
 				System.out.println("== 게시물 작성 ==");
 				int id = lastArticleId + 1;
 				lastArticleId = id;
@@ -156,6 +214,15 @@ public class App {
 			}
 		}
 		return null;
+	}
+	
+	private boolean isLoginIdDup(String loginId) {
+		for(Member member : members) {
+			if(member.loginId.equals(loginId)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private void makeTestData() {
